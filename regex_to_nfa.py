56139deb7be674c501ead2dab3e0c0dcabc2a1cf
alexandercameron <alexandercameron@sandiego.edu>
2017-05-09 09:34:33 -0700

    header

--- regex_to_nfa.py
+++ regex_to_nfa.py
@@ -1,8 +1,8 @@
 '''
-This file contains a Python 2.7 program that determines whether a series of strings is in the language
-described by the corresponding regular expression. This program uses a parse tree to convert from 
-regular expressions to an NFA, and then the NFA is converted to a DFA, which then 
-tests the string, and accepts or rejects.
+This file contains a Python 2.7 program that determines whether a series of strings 
+is in the language described by the corresponding regular expression. 
+A parse tree is used to convert the regular expression to an NFA. 
+The NFA is converted into a DFA, which then runs the string, and accepts or rejects.
 
 Authors: Alex Cameron, Eli Grady, Erick Perez
 COMP 370, Dr. Glick, USD, Spring 17
