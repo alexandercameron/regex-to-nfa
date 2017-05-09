0327863d9c1eeba7aafac705a38d8c2a47b389b6
alexandercameron <alexandercameron@sandiego.edu>
2017-05-09 09:32:14 -0700

    starter file

---
+++ regex_to_nfa.py
@@ -0,0 +1,15 @@
+'''
+This file contains a Python 2.7 program that determines whether a series of strings is in the language
+described by the corresponding regular expression. This program uses a parse tree to convert from 
+regular expressions to an NFA, and then the NFA is converted to a DFA, which then 
+tests the string, and accepts or rejects.
+
+Authors: Alex Cameron, Eli Grady, Erick Perez
+COMP 370, Dr. Glick, USD, Spring 17
+'''
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
